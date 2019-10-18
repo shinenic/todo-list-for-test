d0c20d87d5c71e98fbf5718c510107d44bced1c0
ENTIN WEI <galadiya41@gmail.com>
2019-10-18 13:53:08 +0800

    Updates

---
+++ precache-manifest.f8541da7e784227bc960c51714ffc3b1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dda16e765506681d76744632ee0770f",
+    "url": "/todo-list-for-test/index.html"
+  },
+  {
+    "revision": "6a4581e72f4afaef1af6",
+    "url": "/todo-list-for-test/static/css/main.dc31de75.chunk.css"
+  },
+  {
+    "revision": "803217987a05ea911c14",
+    "url": "/todo-list-for-test/static/js/2.c083a38f.chunk.js"
+  },
+  {
+    "revision": "6a4581e72f4afaef1af6",
+    "url": "/todo-list-for-test/static/js/main.5b5213aa.chunk.js"
+  },
+  {
+    "revision": "d2f51379829105c6d6ad",
+    "url": "/todo-list-for-test/static/js/runtime-main.a2acd1aa.js"
+  },
+  {
+    "revision": "bec9a68a4ae4020503a95d0346c4ac78",
+    "url": "/todo-list-for-test/static/media/add.bec9a68a.svg"
+  },
+  {
+    "revision": "4072bbe3330fb92f95f2aff367a9591e",
+    "url": "/todo-list-for-test/static/media/check.4072bbe3.svg"
+  },
+  {
+    "revision": "1f33e75d7b7df995b1609f8573ac8c9b",
+    "url": "/todo-list-for-test/static/media/github.1f33e75d.svg"
+  },
+  {
+    "revision": "6724e28006e6914078b7503f0dc09f56",
+    "url": "/todo-list-for-test/static/media/remove.6724e280.svg"
+  },
+  {
+    "revision": "3b22be5762ab79f4799108e9b7aced43",
+    "url": "/todo-list-for-test/static/media/undo.3b22be57.svg"
+  }
+]);
